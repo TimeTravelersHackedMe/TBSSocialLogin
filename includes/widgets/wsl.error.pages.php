cc179ee66da4ba019d19ef01d5eae86c3e742763
Brian Zalewski <TimeTravelersHackedMe@users.noreply.github.com>
2016-07-03 01:39:24 -0400

    Update wsl.error.pages.php

--- includes/widgets/wsl.error.pages.php
+++ includes/widgets/wsl.error.pages.php
@@ -67,7 +67,7 @@ if( ! function_exists( 'wsl_render_notice_page' ) )
 				font-family: "Open Sans", sans-serif;
 				margin: 2em auto;
 				padding: 1em 2em;
-				max-width: 616px;
+				max-width: 588px;
 				-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.13);
 				box-shadow: 0 1px 3px rgba(0,0,0,0.13);
 				margin-top: 142px;
